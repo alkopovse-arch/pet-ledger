6140420ddd4d37d27c57e6c31922afb90ebc0aaa
alkopovse-arch <alko.povse@gmail.com>
2026-08-13 13:35:53 +0200

    add footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.scss";
+
+const Footer = (children ) => {
+    return (
+        <footer className="footer">
+            <p>Pet Ledger - 2026</p>
+        </footer>
+    )
+}
+export default Footer;
